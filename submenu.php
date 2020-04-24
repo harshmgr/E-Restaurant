ae6397fca28559c616f866809a91f3e6b5bf44a5
HARSH KUMAR <40995887+harshmgr@users.noreply.github.com>
2020-04-24 13:10:31 +0530

    css updated

--- submenu.php
+++ submenu.php
@@ -21,7 +21,7 @@ while($row = mysqli_fetch_assoc($result)){
 <html>
 	<head>
 		<title>Menu List</title>
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="style.css?version=51">
 		<link rel="stylesheet" type="text/css" href="css/styles.css?version=51">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link href="https://fonts.googleapis.com/css2?family=Baloo+Paaji+2&display=swap" rel="stylesheet">
